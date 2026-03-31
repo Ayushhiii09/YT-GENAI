bfc8cc60e05a82b8de1ded86a0d69d4df34e9b6b
Ayushhiii09 <ayushiisinghh.05@gmail.com>
2026-04-01 00:53:52 +0530

    update API URL

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios instance
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://yt-genai-backend.onrender.com",
     withCredentials: true,
 });
 
